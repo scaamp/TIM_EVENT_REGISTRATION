91f252baf0993077a87064836935304d835ad3d8
sc4mp <58878850+scaamp@users.noreply.github.com>
2022-06-03 11:40:04 +0200

    Inicjacja aplikacji

---
+++ EventRegistration-Backend/src/main/java/tim/eventregistration/EventRegistrationApplication.java
@@ -0,0 +1,21 @@
+package tim.eventregistration;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class EventRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventRegistrationApplication.class, args);
+	}
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
+}
